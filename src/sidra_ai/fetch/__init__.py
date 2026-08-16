43941f667f9399f9f02c563b45396bd7aece0252
tukemen-rgb <tukemen@ramenbu.com>
2026-08-17 06:03:32 +0900

    fetch: export pinned HTTPS transport

--- src/sidra_ai/fetch/__init__.py
+++ src/sidra_ai/fetch/__init__.py
@@ -1,3 +1,11 @@
 from .policy import FetchPolicy, FetchPolicyError, ValidatedFetchTarget
+from .transport import FetchTransportError, PinnedFetchResponse, PinnedHttpsTransport
 
-__all__ = ["FetchPolicy", "FetchPolicyError", "ValidatedFetchTarget"]
+__all__ = [
+    "FetchPolicy",
+    "FetchPolicyError",
+    "FetchTransportError",
+    "PinnedFetchResponse",
+    "PinnedHttpsTransport",
+    "ValidatedFetchTarget",
+]
